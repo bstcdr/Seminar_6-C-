228709efc904b3c10d5d0e12465454c521b0706c
Muxammadali <sherikjumayev1202@gmail.com>
2023-01-14 01:41:09 +0500

    upload Seminar8

--- Seminar8/homework/task4/Program.cs
+++ Seminar8/homework/task4/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0, 0, 0) 25(0, 1, 0)
+// 34(1, 0, 0) 41(1, 1, 0)
+// 27(0, 0, 1) 90(0, 1, 1)
+// 26(1, 0, 1) 55(1, 1, 1)
+
+int[,,] array = new int[2, 2, 2];
+array[0, 0, 0] = 65;
+array[0, 1, 0] = 25;
+array[1, 0, 0] = 34;
+array[1, 1, 0] = 41;
+array[0, 0, 1] = 27;
+array[0, 1, 1] = 90;
+array[1, 0, 1] = 26;
+array[1, 1, 1] = 55;
+
+void PrintArray(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(2); i++)
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1); k++)
+            {
+                Console.Write($"{arr[j, k, i]} ({j}, {k}, {i}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+PrintArray(array);
